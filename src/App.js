531cca8405163e523180d0142e9a3bc2d984ba54
Pradumm <kpradumm@gmail.com>
2024-02-03 15:08:38 +0530

    adding project in github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+// import Header from './components/header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from './pages/home/Home';
+// import About from './pages/home/About'
+// import Contact from './pages/home/contact'
+import Dashboard from './Page/Dashboard'
+import Orderplacetool from './components/Orderplacetool';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} /> */}
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/order" element={<Orderplacetool/>} />
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
   );
 }
 
